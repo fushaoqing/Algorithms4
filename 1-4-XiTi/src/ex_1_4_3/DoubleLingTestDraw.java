6b1f2003360dac434a14754da34b3a7412a8b097
fushaoqing <541718406@qq.com>
2016-11-07 17:28:14 +0800

    1.4.3

--- 1-4-XiTi/src/ex_1_4_3/DoubleLingTestDraw.java
+++ 1-4-XiTi/src/ex_1_4_3/DoubleLingTestDraw.java
@@ -1,8 +1,5 @@
 package ex_1_4_3;
-
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.*;
-
 public class DoubleLingTestDraw {
 	public static double timeTrial(int n){
 		int[] a=new int[n];
@@ -13,9 +10,18 @@ public class DoubleLingTestDraw {
 	}
 
 	public static void main(String[] args) {
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setXscale(0.0,10.0);
+		StdDraw.setYscale(-5.0,1.0);
+		StdDraw.setPenColor(StdDraw.RED);
 		for(int N=250;true;N+=N){
 			double time=DoubleLingTestDraw.timeTrial(N);
-			System.out.printf("%d %5.1fseconds\n",N,time);
+			//StdDraw.point(N,time);
+			double lN=Math.log(N*1.0);
+			double ltime=Math.log(time);
+			if(ltime>=1.0) StdDraw.setCanvasSize(N*2, (int)(time*2));
+			StdDraw.point(lN,ltime);
+			System.out.printf("%5.1f %5.1f seconds\n",lN,ltime);
 		}
 	}
 }
