ed5292537a5b020f6f64c9de4b34b722524b0caf
fushaoqing <541718406@qq.com>
2016-11-08 21:34:50 +0800

    uf

--- 1-4-XiTi/src/ex_1_4_12/Ex_1_4_12.java
+++ 1-4-XiTi/src/ex_1_4_12/Ex_1_4_12.java
@@ -1,5 +1,7 @@
 package ex_1_4_12;
 
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.BinarySearch;
 import edu.princeton.cs.algs4.StdRandom;
 
@@ -15,18 +17,21 @@ public class Ex_1_4_12 {
 	}
 }
 	public static void main(String[] args){
-		//int[] a={1,2,2,3};//null;
-		//int[] b={1,2,3,4};//null;
+		//int[] a={1,2,2,3,5};//null;
+		//int[] b={1,2,3,4,5};//null;
 		int[] a=null;
 		int[] b=null;
-		for(int n=1;true;n+=n){
+		for(int n=250;true;n+=n){
 			a=new int[n];
-			for(int i=0;i<n;i++) a[i]=StdRandom.uniform(-10, 10);
+			for(int i=0;i<n;i++) a[i]=StdRandom.uniform(-100, 100);
 			b=new int[n];
-			for(int j=0;j<n;j++) b[j]=StdRandom.uniform(-10, 10);
+			for(int j=0;j<n;j++) b[j]=StdRandom.uniform(-100, 100);
+			Arrays.sort(a);
+			Arrays.sort(b);
+			StopWatch s=new StopWatch();
 			Ex_1_4_12.count(a, b);
 			System.out.println("");
+			System.out.printf("%d %5.2fseconds  %5.2f\n",n,s.elapsedTime(),s.elapsedTime()/n);
 		}
 	}
-	
 }
