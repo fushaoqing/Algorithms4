2e9142df159cdef5056c7031367231dc82a097ca
fushaoqing <541718406@qq.com>
2017-01-03 22:15:42 +0800

    rabin

--- 5-3-SubstringSearch/src/RabinKarp.java
+++ 5-3-SubstringSearch/src/RabinKarp.java
@@ -1,7 +1,44 @@
 
 public class RabinKarp {
 
+	private String pat;
+	private long patHash;
+	private int M;
 	private long Q;//散列表的大小
-	private int RM;
+	private long RM;
+	private int R=256;
 	
+	public RabinKarp(String pat){
+		this.pat=pat;
+		this.M=pat.length();
+		Q=997;
+		RM=1;
+		for(int i=1;i<M-1;i++)
+			RM=(RM*R)%Q;
+		patHash=hash(pat,M);
+	}
+	
+	private long hash(String key,int M){//返回字符串key0到M-1段的hashcode
+		long h=0;
+		for(int i=0;i<M;i++)
+			h=(h*R+key.charAt(i))%Q;
+		return h;
+	}
+	
+	public int search(String txt){
+		int N=txt.length();
+		long txtHash=hash(txt,M);
+		if(patHash==txtHash) return 0;//一开始就匹配
+		for(int i=M;i<N;i++){
+			txtHash=((txtHash+txt.charAt(i-M)*(Q-RM))*R+txt.charAt(i))%Q;//利用了余数的性质
+		if(patHash==txtHash) 
+			return i-M+1;
+		}
+		return N;
+	}
+	
+	public static void main(String[] args){
+		BoyerMoore b=new BoyerMoore("dabr");
+		System.out.println(b.search("abacadabrac"));
+	}
 }
